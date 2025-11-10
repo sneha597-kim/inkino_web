42bb1ac19164713fbb1a6104ab797276795ec752
Sneha latha <snehalathamandhadi@gmail.com>
2025-11-10 18:47:59 +0530

    Update main.dart

--- inkino_web/lib/main.dart
+++ inkino_web/lib/main.dart
@@ -1,106 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'screens/home_screen.dart';
+import 'widgets/navbar.dart';
 
 void main() {
-  runApp(InKinoApp());
+  runApp(InKinoWeb());
 }
 
-class InKinoApp extends StatelessWidget {
+class InKinoWeb extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'inKino',
+      title: 'inKino Web',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo),
-        textTheme: GoogleFonts.poppinsTextTheme(),
-        useMaterial3: true,
+        primarySwatch: Colors.red,
+        textTheme: Typography.blackMountainView.apply(fontFamily: 'Poppins'),
       ),
-      home: HomeScreen(),
-    );
-  }
-}
-
-class HomeScreen extends StatelessWidget {
-  final List<Map<String, String>> categories = [
-    {'title': 'Now Showing', 'image': 'assets/images/movie1.jpg'},
-    {'title': 'Upcoming', 'image': 'assets/images/movie2.jpg'},
-    {'title': 'Popular', 'image': 'assets/images/movie3.jpg'},
-    {'title': 'Favorites', 'image': 'assets/images/movie4.jpg'},
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('🎬 inKino'),
-        centerTitle: true,
-        backgroundColor: Colors.indigo,
-        foregroundColor: Colors.white,
-      ),
-      body: GridView.builder(
-        padding: const EdgeInsets.all(16),
-        itemCount: categories.length,
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          crossAxisSpacing: 12,
-          mainAxisSpacing: 12,
-        ),
-        itemBuilder: (context, index) {
-          final category = categories[index];
-          return InkWell(
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (_) => MovieListScreen(category['title']!),
-                ),
-              );
-            },
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(15),
-                image: DecorationImage(
-                  image: AssetImage(category['image']!),
-                  fit: BoxFit.cover,
-                  colorFilter: ColorFilter.mode(
-                      Colors.black.withOpacity(0.4), BlendMode.darken),
-                ),
-              ),
-              alignment: Alignment.center,
-              child: Text(
-                category['title']!,
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontSize: 22,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-          );
-        },
-      ),
-    );
-  }
-}
-
-class MovieListScreen extends StatelessWidget {
-  final String categoryName;
-  const MovieListScreen(this.categoryName, {super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(categoryName),
-        backgroundColor: Colors.indigo,
-        foregroundColor: Colors.white,
-      ),
-      body: Center(
-        child: Text(
-          'Movies under "$categoryName" coming soon...',
-          style: const TextStyle(fontSize: 18),
-        ),
+      home: Scaffold(
+        appBar: const NavBar(title: "inKino Web"),
+        body: HomeScreen(),
       ),
     );
   }
