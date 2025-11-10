37f2b720f6a0cc3161dd8d68ab6b6288f483eaef
Sneha latha <snehalathamandhadi@gmail.com>
2025-11-10 18:45:00 +0530

    Update movie.dart

---
+++ inkino_web/lib/models/movie.dart
@@ -0,0 +1,15 @@
+class Movie {
+  final String title;
+  final String genre;
+  final String time;
+  final String description;
+  final String image;
+
+  Movie({
+    required this.title,
+    required this.genre,
+    required this.time,
+    required this.description,
+    required this.image,
+  });
+}
